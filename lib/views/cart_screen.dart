ec898551dfd459cced9ddd768f967de9d482b175
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-05-21 18:22:46 -0300

    Itens do Carrinho

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart';
+import '../widgets/cart_item_widget.dart';
 
 class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     final Cart cart = Provider.of(context);
+    final cartItems = cart.items.values.toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Carrinho'),
@@ -47,6 +51,13 @@ class CartScreen extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.itemsCount,
+              itemBuilder: (ctx, i) => CartItemWidget(cartItems[i]),
             ),
           ),
         ],
