06e90b3a94980707326f5c4226052120e0c68710
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-05-21 10:43:24 -0300

    ChangeNotifierProvider.value

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -17,8 +17,8 @@ class ProductGrid extends StatelessWidget {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (ctx, i) => products[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: products[i],
         child: ProductItem(),
       ),
     );
