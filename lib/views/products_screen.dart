cffaa97be88bde5d20302570148fcba0bea2dcdc
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-05-22 05:25:15 -0300

    Componente Gerenciamento de Produto #01

---
+++ lib/views/products_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/utils/app_routes.dart';
+
+import '../widgets/app_drawer.dart';
+import '../providers/products.dart';
+
+class ProductsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final products = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gerenciar Produtos'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                AppRoutes.PRODUCTS,
+              );
+            },
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: products.itemsCount,
+          itemBuilder: (ctx, i) => Text(
+            'teste'
+          ),
+        ),
+      ),
+    );
+  }
+}
