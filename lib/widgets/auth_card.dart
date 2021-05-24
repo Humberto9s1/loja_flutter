35db1e9e947a486aa491cc695435c321ad622364
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-05-24 16:43:53 -0300

    Tela de Autenticação #04

--- lib/widgets/auth_card.dart
+++ lib/widgets/auth_card.dart
@@ -18,7 +18,37 @@ class _AuthCardState extends State<AuthCard>{
   };
 
   void _submit(){
+    if(!_form.currentState.validate()){
+      return;
+    }
 
+    setState(() {
+      _isLoading = true;
+    });
+
+    _form.currentState.save();
+
+    if(_authMode == AuthMode.Login){
+      //Login
+    } else {
+      //Registrar
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _swichAuthMode(){
+    if(_authMode == AuthMode.Login){
+      setState(() {
+        _authMode = AuthMode.Signup;
+      });
+    } else {
+      setState(() {
+        _authMode = AuthMode.Login;
+      });
+    }
   }
 
   @override
@@ -30,68 +60,77 @@ class _AuthCardState extends State<AuthCard>{
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Container(
-        height: 220,
-        width: deviceSize.width * 0.75,
-        child: Form(
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value.isEmpty || !value.contains('@')) {
-                    return "Informe um e-mail válido!";
-                  }
-                  return null;
-                },
-                onSaved: (value) => _authData['email'] = value,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Senha'),
-                controller: _passwordController,
-                obscureText: true,
-                validator: (value) {
-                  if (value.isEmpty || value.length < 5) {
-                    return "Informe uma senha válida!";
-                  }
-                  return null;
-                },
-                onSaved: (value) => _authData['password'] = value,
-              ),
-              if (_authMode == AuthMode.Signup)
+      child: SingleChildScrollView(
+        child: Container(
+          height: _authMode == AuthMode.Login ? 250 : 320,
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            child: Column(
+              children: [
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value.isEmpty || !value.contains('@')) {
+                      return "Informe um e-mail válido!";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _authData['email'] = value,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Senha'),
+                  controller: _passwordController,
                   obscureText: true,
-                  validator: _authMode == AuthMode.Signup
-                      ? (value) {
-                          if (value != _passwordController.text) {
-                            return "Senha são diferentes!";
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 5) {
+                      return "Informe uma senha válida!";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _authData['password'] = value,
+                ),
+                if (_authMode == AuthMode.Signup)
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                    obscureText: true,
+                    validator: _authMode == AuthMode.Signup
+                        ? (value) {
+                            if (value != _passwordController.text) {
+                              return "Senha são diferentes!";
+                            }
+                            return null;
                           }
-                          return null;
-                        }
-                      : null,
+                        : null,
+                  ),
+                Spacer(),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).primaryTextTheme.button.color,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 8.0,
+                    ),
+                    child: Text(
+                      _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR',
+                    ),
+                    onPressed: _submit,
+                  ),
+                FlatButton(onPressed: _swichAuthMode,
+                    child: Text(
+                      _authMode == AuthMode.Login ? 'REGISTRAR' : 'LOGIN',
+                    ),
+                  textColor: Theme.of(context).primaryColor,
                 ),
-              Spacer(),
-              if (_isLoading)
-                CircularProgressIndicator()
-              else
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  color: Theme.of(context).primaryColor,
-                  textColor: Theme.of(context).primaryTextTheme.button.color,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 30.0,
-                    vertical: 8.0,
-                  ),
-                  child: Text(
-                    _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR',
-                  ),
-                  onPressed: _submit,
-                ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
