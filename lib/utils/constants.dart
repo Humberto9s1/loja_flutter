75503f5841179451464ea474ee9abaf86885f3aa
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-05-24 20:25:18 -0300

    App com login

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,3 @@
 class Constants {
-  static const BASE_API_URL = 'https://flutter-hbt-default-rtdb.firebaseio.com';
+  static const BASE_API_URL = 'https://flutter-hbt-default-rtdb.firebaseio.com/';
 }
